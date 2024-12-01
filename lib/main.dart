4822f7b298334af0c2ff30a6449b2b246d9bb9dd
brughwani <brughwani@gmail.com>
2024-12-01 11:43:02 +0530

    added dynamic filtering of employees

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'loginpage.dart';
 import 'Admindashboard.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'employeeprovider.dart';
 Future<void> main() async {
  
 
-  runApp(MyWidget());
+  runApp(
+      ChangeNotifierProvider(
+        create: (_) => EmployeeProvider(),
+
+      child: MyWidget()));
 }
 
 class MyWidget extends StatelessWidget {

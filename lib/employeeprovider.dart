23a5465a99cf252deaef18f74f46196ff60d29bd
brughwani <brughwani@gmail.com>
2024-12-15 09:07:54 +0530

    bug fixes

--- lib/employeeprovider.dart
+++ lib/employeeprovider.dart
@@ -3,13 +3,17 @@ import 'dart:convert';
 import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+
+
 class EmployeeProvider with ChangeNotifier {
   List<String> _selectedFields = []; // List to store selected fields
   List<Map<String, dynamic>> _employees = []; // List to store employee data
   Map<String, bool> _checkboxStatuses = {}; // Current statuses of checkboxes
 
   List<String> get selectedFields => _selectedFields;
+
   List<Map<String, dynamic>> get employees => _employees;
+
   Future<void> loadInitialCheckboxStatuses(List<String> fields) async {
     for (var field in fields) {
       _checkboxStatuses[field] = false; // Default status is unchecked
@@ -30,17 +34,36 @@ class EmployeeProvider with ChangeNotifier {
     notifyListeners(); // Notify listeners (UI) to rebuild
   }
 
-  // Method to fetch employees based on selected fields
-  Future<void> fetchEmployees(List<String> fields) async {
-    final url = "https://crmvercelfun.vercel.app/api/getallemployee?fields[]=${fields.join('&fields[]=')}";
-    final response = await http.get(Uri.parse(url), headers: {'Content-Type': 'application/json'});
+  Future<List<Map<String, dynamic>>> fetchEmployees(List<String> fields) async {
+    final url = "https://crmvercelfun.vercel.app/api/getallemployee?fields[]=${fields}";
+    final response = await http.get(
+        Uri.parse(url), headers: {'Content-Type': 'application/json'});
 
     if (response.statusCode == 200) {
       final List<dynamic> jsonData = jsonDecode(response.body);
-      _employees = jsonData.map((e) => Map<String, dynamic>.from(e)).toList();
-      notifyListeners(); // Notify listeners to rebuild the UI with new data
+      final List<Map<String, dynamic>> fetchedEmployees = jsonData.map((
+          e) => Map<String, dynamic>.from(e)).toList();
+
+      // Update internal state
+      _employees = fetchedEmployees;
+      notifyListeners(); // Update UI
+      return fetchedEmployees; // Return fetched data
     } else {
       throw Exception('Failed to fetch employee data');
     }
   }
 }
+  // Method to fetch employees based on selected fields
+//   Future<void> fetchEmployees(List<String> fields) async {
+//     final url = "https://crmvercelfun.vercel.app/api/getallemployee?fields[]=${fields}";
+//     final response = await http.get(Uri.parse(url), headers: {'Content-Type': 'application/json'});
+//
+//     if (response.statusCode == 200) {
+//       final List<dynamic> jsonData = jsonDecode(response.body);
+//       _employees = jsonData.map((e) => Map<String, dynamic>.from(e)).toList();
+//       notifyListeners(); // Notify listeners to rebuild the UI with new data
+//     } else {
+//       throw Exception('Failed to fetch employee data');
+//     }
+//   }
+// }
